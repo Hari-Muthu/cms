d908d63e45f44af1ff5d376979a24606d14a5a22
HARIMUTHU-M <harimuthutce@gmail.com>
2022-07-30 18:15:14 +0530

    placeOrder fn: cna't bring the data outside the findOne()

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -2,8 +2,10 @@ const UserModel = require('../models/user-model');
 const MenuModel = require("../models/menu-model");
 const OrderModel = require("../models/order-model");
 
-getMenu = async(req,res) => {
-    await MenuModel.find({},(err,menuItems) => {
+
+
+getMenu = (req,res) => {
+    MenuModel.find({},(err,menuItems) => {
         if(err){
             return res.status(400).json({success:false, error:err})
         }
@@ -13,33 +15,75 @@ getMenu = async(req,res) => {
                 error: "No items found"
             })
         }
-        return res.status(200).json({
-            success: true,
-            data: menuItems.filter((item)=> item.stock!=0)
-        })
-    }).catch(err => console.log(err))
+        else{
+            return res.status(200).json({
+                success: true,
+                data: menuItems.filter((item)=> item.stock!=0)
+            })
+        }
+    })
+    // .catch(err => console.log(err))
 }
 
 placeOrder = (req,res) => {
+    
     const {email,orderedItems} = req.body;
+    
+    
+    // const name = orderedItems.map((item)=>return {name:item.itemName,quantity:item.quantity});
+    // console.log(name);
+    // const quantity = orderedItems.map((item)=> item.quantity);
+    // console.log(quantity);
+    
+    // console.log(email)
+
+    var currentOrderItem  = []
+
+    
+     orderedItems.forEach((item)=>{
+
+        
+        MenuModel.findOne({name:item.itemName},(err,menuItems)=>{
+            if(err){
+                return res.status(400).json({success:false, error:err})
+            }
+            else if(!menuItems){
+                return res.status(404).json({success: false,error: "Item not found"})
+            }
+            else{
+                // console.log(menuItems);
+                const newStock = menuItems.stock - item.quantity;
+                if (newStock > 0 ){
+                // menuItems.save();
+                }
+
+                // console.log("In else statement")
+                const price = menuItems.price*item.quantity;
+
+                const obj={
+                    name: menuItems.name,
+                    quantity: item.quantity,
+                    price: price
+                }
+                currentOrderItem.push(obj);
+                // console.log(currentOrderItem);
+
+            }
+        })
+    })
+
     const newOrder = new OrderModel({
         email,
-        orderedItems
+        currentOrderItem
     })
-    newOrder.save((err,order) => {
+
+    newOrder.save((err,order)=>{
         if(err){
             return res.status(400).json({success:false, error:err})
         }
-        else if(!order){
-            return res.status(404).json({
-                success: false,
-                error: "No items found"
-            })
+        else{
+            return res.status(200).json({success: true,data: order})
         }
-        return res.status(200).json({
-            success: true,
-            data: order
-        })
     })
 }
 
@@ -56,7 +100,8 @@ login = (req,res) => {
             return res.status(404).json({success: false,error: "Password incorrect"})
         }
         return res.status(200).json({success: true,data: user})
-}).catch(err => console.log(err))}
+})}
+// .catch(err => console.log(err))}
 
 register = (req,res) => {
     const {name,email,phoneno,password} = req.body;
