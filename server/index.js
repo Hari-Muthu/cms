2cc0486fc121ed2134a4c26ffd433db0403f9c07
HARIMUTHU-M <harimuthutce@gmail.com>
2022-08-09 20:31:49 +0530

    Updated portNo with .env

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const db = require("./db/db")
 const adminRouter = require("./routes/admin-router");
 const userRouter = require("./routes/user-router");
+require("dotenv").config();
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/",(req,res) => {
 app.use("/adminApi",adminRouter)
 app.use("/userApi",userRouter)
 
-app.listen(5000,()=>{
-    console.log("Server started at port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`);
 })
